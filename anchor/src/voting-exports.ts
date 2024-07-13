173bb3e370e649cee629abb5752ae2b25b10395a
Priyesh-Kun <122373342+Priyesh-Kun@users.noreply.github.com>
2024-07-13 21:04:52 +0530

    Update voting-exports.ts

--- anchor/src/voting-exports.ts
+++ anchor/src/voting-exports.ts
@@ -2,11 +2,10 @@
 import { AnchorProvider, Program } from '@coral-xyz/anchor';
 import { PublicKey } from '@solana/web3.js';
 import type { Voting } from '../target/types/voting';
-import { Voting as VotingIDL } from '../target/types/voting';
 import idl from "../target/idl/voting.json"
 
 // Re-export the generated IDL and type
-export { Voting, VotingIDL };
+export { Voting };
 
 // The programId is imported from the program IDL.
 export const VOTING_PROGRAM_ID = new PublicKey(idl.address);
