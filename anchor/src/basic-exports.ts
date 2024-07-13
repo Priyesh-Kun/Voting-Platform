e18cc9ff6d215c698c0ff162ee5af2206be79c07
Priyesh-Kun <122373342+Priyesh-Kun@users.noreply.github.com>
2024-07-13 21:04:37 +0530

    Update basic-exports.ts

--- anchor/src/basic-exports.ts
+++ anchor/src/basic-exports.ts
@@ -2,11 +2,10 @@
 import { AnchorProvider, Program } from '@coral-xyz/anchor';
 import { PublicKey } from '@solana/web3.js';
 import type { Basic } from '../target/types/basic';
-import { Basic as BasicIDL } from '../target/types/basic';
 import idl from "../target/idl/basic.json"
 
 // Re-export the generated IDL and type
-export { Basic, BasicIDL };
+export { Basic };
 
 // The programId is imported from the program IDL.
 export const BASIC_PROGRAM_ID = new PublicKey(idl.address);
